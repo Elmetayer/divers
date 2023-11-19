ffde63a8c5de86cd3819773466bb69ba58353dd3
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2023-11-19 15:58:48 +0100

    Update streamlit_secret_santa.py

--- streamlit_secret_santa.py
+++ streamlit_secret_santa.py
@@ -4,8 +4,8 @@ from io import StringIO
 import json
 
 uploaded_participants_file = st.file_uploader('Participants', type = 'csv', accept_multiple_files = False)
-if ploaded_participants_file is not None:
-    df_participants = pd.read_csv(ploaded_participants_file)
+if uploaded_participants_file is not None:
+    df_participants = pd.read_csv(uploaded_participants_file)
     st.write(df_participants)
 
 uploaded_config_file = st.file_uploader('Configuration', type = 'json', accept_multiple_files = False)
