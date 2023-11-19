956cafb8b31bb9151999cb99095a7bd57916da26
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2023-11-19 18:30:00 +0100

    Update streamlit_secret_santa.py

--- streamlit_secret_santa.py
+++ streamlit_secret_santa.py
@@ -48,12 +48,12 @@ def get_file_downloader_html(bin_file, file_label='File'):
 
 st.title('Générateur secret santa')
 
-get_file_downloader_html('participants_exemple.csv', 'participants.csv')
-
 uploaded_participants_file = st.file_uploader('Participants', type = 'csv', accept_multiple_files = False, help = 'liste des noms des participants au format csv')
 if uploaded_participants_file is not None:
     df_participants = pd.read_csv(uploaded_participants_file, header=None, names=['participants'])
     liste_participants = df_participants['participants'].to_list()
+
+get_file_downloader_html('participants_exemple.csv', 'participants.csv')
 
 uploaded_config_file = st.file_uploader('Configuration', type = 'json', accept_multiple_files = False, 
                                         help = 'fichier json avec une clé "exclusions" et une clé "obligations" qui contiennent une liste de couples (offrant, recevant)')
@@ -61,7 +61,6 @@ if uploaded_config_file is not None:
     config = json.load(uploaded_config_file)
 
 get_file_downloader_html('config_exemple.json', 'config.json')
-
 
 if st.button('Générer') and liste_participants is not None:
     resultats = secret_santa(liste_participants, config)
