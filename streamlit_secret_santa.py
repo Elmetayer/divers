2281e4c1c98671f2227297668295a49ed9d6fc6b
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2023-11-20 17:14:04 +0100

    Mise à jour

--- streamlit_secret_santa.py
+++ streamlit_secret_santa.py
@@ -24,7 +24,7 @@ with open(file_config_exemple) as f:
     config_exemple = json.load(f)
 
 # fonction pour générer les résultats du secret santa
-def secret_santa(liste_participants, config):
+def secret_santa_graph(liste_participants, config):
     '''
     la fonction prend en arguments une liste de participants, et une configuration qui définit:
     - les relations offrant > recevant prédéfinies
@@ -34,28 +34,40 @@ def secret_santa(liste_participants, config):
     # création du graphe avec les participants
     exclusions = config['exclusions'].copy()
     edge_list = pd.DataFrame(
-        [(item[0], item[1]) for item in itertools.product(liste_participants, liste_participants) if item[0] != item[1] and [item[0], item[1]] not in exclusions], 
-        columns = ['offrant', 'recevant'])
+        [[item[0], item[1], 0] for item in itertools.product(liste_participants, liste_participants) if item[0] != item[1] and [item[0], item[1]] not in exclusions], 
+        columns = ['source', 'target', 'retenu'])
     G = nx.from_pandas_edgelist(
         edge_list, 
-        source = 'offrant',
-        target =  'recevant',
+        edge_attr = 'retenu',
         create_using = nx.DiGraph())
+    
     # on traite d'abord les obligations
     obligations = config['obligations'].copy()
-    offrants_restant = liste_participants.copy()
+    offrants_obliges = []
+    recevants_obliges = []
     for offrant, recevant in obligations:
-        # on enlève toutes les arrêtes qui ne sont pas celle de l'obligation
-        G.remove_edges_from([edge for edge in G.edges if ((edge[0] == offrant and edge[1] != recevant) or (edge[0] != offrant and edge[1] == recevant))])
-        offrants_restant.pop(offrants_restant.index(offrant))
+        nx.set_edge_attributes(G, {(offrant, recevant): {'retenu': 1}})
+        offrants_obliges.append(offrant)
+        recevants_obliges.append(recevant)
+       
     # on parcours au hasard la liste des participants
-    for i_offrant_a_traiter in np.random.permutation(len(offrants_restant)):
-        offrant_a_traiter = offrants_restant[i_offrant_a_traiter]
-        # on tire au hasard les recevants restants parmi les target 
-        recevant_a_traiter = list(G[offrant_a_traiter])[np.random.randint(len(G[offrant_a_traiter]))]
-        # on enlève toutes les arrêtes qui ne sont pas celle retenue
-        G.remove_edges_from([edge for edge in G.edges if ((edge[0] == offrant_a_traiter and edge[1] != recevant_a_traiter) or (edge[0] != offrant_a_traiter and edge[1] == recevant_a_traiter))])
-    return(list(G.edges))
+    cadeaux_a_faire = len(liste_participants) - len(obligations)
+    parcours_offrant = []
+    parcours_recevant = []
+    while len(parcours_offrant) < cadeaux_a_faire and len(parcours_recevant) < cadeaux_a_faire:
+        offrants_possibles = [offrant for offrant in liste_participants if (offrant not in parcours_offrant and offrant not in offrants_obliges)]
+        offrant_a_traiter = offrants_possibles[np.random.randint(len(offrants_possibles))]
+        recevants_possibles = [recevant for recevant in list(G[offrant_a_traiter]) if (recevant not in parcours_recevant and recevant not in recevants_obliges)]
+        if len(recevants_possibles) > 0:
+            # on tire au hasard les recevants restants parmi les target 
+            recevant_a_traiter = recevants_possibles[np.random.randint(len(recevants_possibles))]
+            nx.set_edge_attributes(G, {(offrant_a_traiter, recevant_a_traiter): {'retenu': 1}})
+            parcours_offrant.append(offrant_a_traiter)
+            parcours_recevant.append(recevant_a_traiter)
+        else:
+            # retour arrière
+            nx.set_edge_attributes(G, {(parcours_offrant.pop(), parcours_recevant.pop()): {'retenu': 0}})
+    return([(offrant, recevant) for offrant, recevant, edge in G.edges(data=True) if edge['retenu'] == 1])
 
 # fonction qui génère un lien de téléchargement
 def get_file_downloader_html(bin_file, file_label):
