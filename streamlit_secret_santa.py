0a68a4015a65a2ad68240125b7dfd673475733e1
Bruno Lemetayer <bruno.lemetayer@rte-france.com>
2023-11-20 09:54:20 +0100

    Update streamlit_secret_santa.py

--- streamlit_secret_santa.py
+++ streamlit_secret_santa.py
@@ -6,13 +6,28 @@ import json
 import os
 import base64
 
+# données uploadées, à None par défaut
 liste_participants = None
 config = {
     'exclusions':[],
     'obligations':[]}
 df_resultats = None
 
+# données exemples
+file_participant_exemple = 'participants.csv'
+df_participant_exemple = pd.read_csv(file_participant_exemple, header=None, names=['participants'])
+liste_participants_exemple = df_participant_exemple['participants'].to_list()
+file_config_exemple = 'config.json'
+config_exemple = json.load(file_config_exemple)
+
+# fonction pour générer les résultats du secret santa
 def secret_santa(liste_participants, config):
+    '''
+    la fonction prend en arguments une liste de participants, et une configuration qui définit:
+    - les relations offrant > recevant prédéfinies
+    - les relations offrant > recevant à exclure  
+    elle renvoie une liste de couples (offrant, recevant)
+    '''
     resultats = []
     offrants_restant = liste_participants.copy()
     recevants_restant = liste_participants.copy()
@@ -39,6 +54,7 @@ def secret_santa(liste_participants, config):
                 continue
     return(resultats)
 
+# fonction qui génère un lien de téléchargement
 def get_file_downloader_html(bin_file, file_label='File'):
     with open(bin_file, 'rb') as f:
         data = f.read()
@@ -46,26 +62,37 @@ def get_file_downloader_html(bin_file, file_label='File'):
     href = f'<a href="data:application/octet-stream;base64,{bin_str}" download="{os.path.basename(bin_file)}">télécharger un exemple {file_label}</a>'
     st.markdown(href, unsafe_allow_html=True)
 
+# titre de l'application
 st.title('Générateur secret santa')
 
+# upload du fichier avec la liste des participants
 uploaded_participants_file = st.file_uploader('Participants', type = 'csv', accept_multiple_files = False, help = 'liste des noms des participants au format csv')
 if uploaded_participants_file is not None:
     df_participants = pd.read_csv(uploaded_participants_file, header=None, names=['participants'])
     liste_participants = df_participants['participants'].to_list()
 
-get_file_downloader_html('participants_exemple.csv', 'participants.csv')
+# téléchargement d'un fichier exemple de liste de participants
+get_file_downloader_html('participants_exemple.csv', file_participant_exemple)
 
+# uload du fichier de configuration
 uploaded_config_file = st.file_uploader('Configuration', type = 'json', accept_multiple_files = False, 
                                         help = 'fichier json avec une clé "exclusions" et une clé "obligations" qui contiennent une liste de couples (offrant, recevant)')
 if uploaded_config_file is not None:
     config = json.load(uploaded_config_file)
 
-get_file_downloader_html('config_exemple.json', 'config.json')
+# téléchargement d'un fichier exemple de configuration
+get_file_downloader_html('config_exemple.json', )
 
-if st.button('Générer') and liste_participants is not None:
-    resultats = secret_santa(liste_participants, config)
+# bouton pour lancer le calcul du secret santa
+if st.button('Générer') :
+    if liste_participants is not None:
+        resultats = secret_santa(liste_participants, config)
+        st.write('résultats:')
+    else:
+        # s'il n'y a pas de fichier uploadé, on génère avec les exemples
+        resultats = secret_santa(liste_participants_exemple, config_exemple)
+        st.write('pas de fichiers uploadés, résultats avec les données exemple:')
     df_resultats = pd.DataFrame(resultats, columns = ['offrant', 'recevant'])
-    st.write('résultats:')
     st.write(df_resultats)
     st.download_button(
         label = 'télécharger les résultats',
